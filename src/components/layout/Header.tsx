77ef85eb160103cfb723c615f8504556b946c914
Albin Aji <albinpariyarathu@gmail.com>
2024-12-22 18:24:00 +0530

    build error fixed in header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { motion, AnimatePresence } from "framer-motion";
-import { Menu, X } from "lucide-react";
+import { Menu } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import {
   Sheet,
